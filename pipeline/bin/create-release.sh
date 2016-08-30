3d15eefe9758aa5e1665b03af4786f0603e59da7
Szymon Kodrebski <szymon.kodrebski@sap.com>
2016-08-30 09:54:38 +0200

    test

--- pipeline/bin/create-release.sh
+++ pipeline/bin/create-release.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 set -e
-bosh target 192.168.50.4 lite
-bosh login admin admin
-bosh create release --name wordpress --version 1.0
-bosh upload release
-bosh upload stemcell https://bosh.io/d/stemcells/bosh-warden-boshlite-ubuntu-trusty-go_agent\?v\=3147
-bosh deployment examples/wordpress.yml
-bosh deploy
+bosh -t https://192.168.50.4:25555 -u admin -p admin create release --name wordpress --version 1.0
+bosh -t https://192.168.50.4:25555 -u admin -p admin upload release
+bosh -t https://192.168.50.4:25555 -u admin -p admin upload stemcell https://bosh.io/d/stemcells/bosh-warden-boshlite-ubuntu-trusty-go_agent\?v\=3147
+bosh -t https://192.168.50.4:25555 -u admin -p admin deployment examples/wordpress.yml
+bosh -t https://192.168.50.4:25555 -u admin -p admin deploy
