21f6200c9adbec4b368eb9dc4e2498e19c530ed4
Szymon Kodrebski <szymon.kodrebski@sap.com>
2016-08-30 09:56:27 +0200

    test

--- pipeline/bin/create-release.sh
+++ pipeline/bin/create-release.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 set -e
+cd git-repo
 bosh -t https://192.168.50.4:25555 -u admin -p admin create release --name wordpress --version 1.0
 bosh -t https://192.168.50.4:25555 -u admin -p admin upload release
 bosh -t https://192.168.50.4:25555 -u admin -p admin upload stemcell https://bosh.io/d/stemcells/bosh-warden-boshlite-ubuntu-trusty-go_agent\?v\=3147
