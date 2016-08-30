5ac639ff8b77f5651d08d46454a685dfbd367917
Szymon Kodrebski <szymon.kodrebski@sap.com>
2016-08-30 09:47:08 +0200

    test

--- pipeline/bin/create-release.sh
+++ pipeline/bin/create-release.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 set -e
-PWD=`pwd`
-DIRLS=`ls git-repo/examples`
-touch test.txt
-echo $PWD >>test.txt
-echo $DIRLS >>test.txt
+bosh target 192.168.50.4 lite
+bosh login admin admin
+bosh create release --name wordpress --version 1.0
+bosh upload release
+bosh upload stemcell https://bosh.io/d/stemcells/bosh-warden-boshlite-ubuntu-trusty-go_agent\?v\=3147
+bosh deployment examples/wordpress.yml
+bosh deploy
