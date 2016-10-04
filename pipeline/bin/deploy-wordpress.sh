e72da518c9813523ff53b929bf41afae1919ebba
Szymon Kodrebski <szymon.kodrebski@sap.com>
2016-10-04 13:37:44 +0200

    applied changes to pipeline

--- pipeline/bin/deploy-wordpress.sh
+++ pipeline/bin/deploy-wordpress.sh
@@ -4,7 +4,7 @@ set -e
 cd git-repo
 
 bosh -t $BOSH_TARGET -u $BOSH_USER -p $BOSH_PASSWORD create release --name wordpress --version 1.0
-bosh -t $BOSH_TARGET -u $BOSH_USER -p $BOSH_PASSWORD upload release dev_releases/wordpress/wordpress-0+dev.1.yml
+bosh -t $BOSH_TARGET -u $BOSH_USER -p $BOSH_PASSWORD upload release dev_releases/wordpress/wordpress-1.0.yml
 bosh -t $BOSH_TARGET -u $BOSH_USER -p $BOSH_PASSWORD upload stemcell https://bosh.io/d/stemcells/bosh-warden-boshlite-ubuntu-trusty-go_agent\?v\=3147
 bosh -t $BOSH_TARGET -u $BOSH_USER -p $BOSH_PASSWORD deployment examples/wordpress.yml
 bosh -t $BOSH_TARGET -u $BOSH_USER -p $BOSH_PASSWORD deploy
